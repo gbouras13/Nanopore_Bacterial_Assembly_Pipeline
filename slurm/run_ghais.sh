4b446a9f5f40745ffafef9ed6dd9ff293561c179
gbouras13 <george.bouras@adelaide.edu.au>
2022-05-27 15:28:08 +0930

    metadata and slurm

--- slurm/run_ghais.sh
+++ slurm/run_ghais.sh
@@ -12,11 +12,11 @@
 #SBATCH -p batch
 #SBATCH -N 1                                                    # number of tasks (sequential job starts 1 task) (check this if your job unexpectedly uses 2 nodes)
 #SBATCH -c 1                                                    # number of cores (sequential job calls a multi-thread program that uses 8 cores)
-#SBATCH --time=08:00:00                                         # time allocation, which has the format (D-HH:MM), here set to 1 hou                                           # generic resource required (here requires 1 GPUs)
-#SBATCH --mem=10GB                                              # specify memory required per node
+#SBATCH --time=2-23:00:00                                         # time allocation, which has the format (D-HH:MM), here set to 1 hou                                           # generic resource required (here requires 1 GPUs)
+#SBATCH --mem=1GB                                              # specify memory required per node
 
 
-SNK_DIR="/hpcfs/users/a1667917/Bacteria_Multiplex/Nanopore_Bacterial_Assembly_Pipeline"
+SNK_DIR="/hpcfs/users/a1667917/Ghais/S_Aureus_Polished/Nanopore_Bacterial_Assembly_Pipeline"
 PROF_DIR="/hpcfs/users/a1667917/snakemake_slurm_profile"
 
 cd $SNK_DIR
@@ -25,10 +25,11 @@ module load Anaconda3/2020.07
 conda activate snakemake_clean_env
 
 snakemake -c 1 -s runner.smk --use-conda --profile $PROF_DIR/bact_assembly \
---config csv=/hpcfs/users/a1667917/Bacteria_Multiplex/Nanopore_Bacterial_Assembly_Pipeline/hpc_metadata.csv Output=/hpcfs/users/a1667917/Bacteria_Multiplex/Pipeline_Out Polypolish_Dir=/hpcfs/users/a1667917/Polypolish 
+--config csv=ghais_hpc_metadata.csv Output=/hpcfs/users/a1667917/Ghais/S_Aureus_Polished/Assembly_Output Polypolish_Dir=/hpcfs/users/a1667917/Polypolish 
 
 
-#snakemake -c 1 -s runner.smk --use-conda --conda-create-envs-only --conda-frontend conda  \
-#--config csv=/hpcfs/users/a1667917/Bacteria_Multiplex/Nanopore_Bacterial_Assembly_Pipeline/hpc_metadata.csv Output=/hpcfs/users/a1667917/Bacteria_Multiplex/Pipeline_Out Polypolish_Dir=/hpcfs/users/a1667917/Polypolish 
+# snakemake -c 1 -s runner.smk --use-conda --conda-create-envs-only --conda-frontend conda  \
+# --config csv=ghais_hpc_metadata.csv Output=/hpcfs/users/a1667917/Ghais/S_Aureus_Polished/Assembly_Output Polypolish_Dir=/hpcfs/users/a1667917/Polypolish 
+
 
 conda deactivate
