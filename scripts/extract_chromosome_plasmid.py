397f905d3b18eaa749d0f75cd6a81bd4ab840935
gbouras13 <george.bouras@adelaide.edu.au>
2022-05-30 11:32:38 +0930

    script change

--- scripts/extract_chromosome_plasmid.py
+++ scripts/extract_chromosome_plasmid.py
@@ -6,11 +6,11 @@ def parse_fastas( input_fasta, chromosome_fasta, plasmid_fasta):
     # read in the fasta
     with open(chromosome_fasta, 'w') as fa:
         for dna_record in SeqIO.parse(input_fasta, "fasta"):
-            if dna_record.id == "contig_1_polypolish":
+            if dna_record.id == "contig_1":
                 SeqIO.write(dna_record, fa, 'fasta')
     with open(plasmid_fasta, 'w') as fa:
         for dna_record in SeqIO.parse(input_fasta, "fasta"):
-            if dna_record.id != "contig_1_polypolish":
+            if dna_record.id != "contig_1":
                 SeqIO.write(dna_record, fa, 'fasta')
             
 parse_fastas(snakemake.input[0],snakemake.output[0], snakemake.output[1])
