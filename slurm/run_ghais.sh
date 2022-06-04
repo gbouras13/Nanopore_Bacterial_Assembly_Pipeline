8cd4ceef060d4b0cc2f2cc72562f167d0fa6e992
gbouras13 <84495559+gbouras13@users.noreply.github.com>
2022-06-04 23:53:40 +0930

    Update run_ghais.sh

--- slurm/run_ghais.sh
+++ slurm/run_ghais.sh
@@ -21,15 +21,13 @@ PROF_DIR="/hpcfs/users/a1667917/snakemake_slurm_profile"
 
 cd $SNK_DIR
 
-#module load Anaconda3/2020.07
-#conda activate snakemake_clean_env
+module load Anaconda3/2020.07
+conda activate snakemake_clean_env
 
 
 
 # needs snakemake minimal 6.13. Don't know why, Snakemake 7 breaks with a strange error about the output files not existing and an sacct error
 
-# activates fresh miniconda
-eval "$(/home/a1667917/Conda_3.8/bin/conda shell.bash hook)"
 
 
 snakemake -c 1 -s runner.smk --use-conda --conda-frontend conda --profile $PROF_DIR/bact_assembly \
