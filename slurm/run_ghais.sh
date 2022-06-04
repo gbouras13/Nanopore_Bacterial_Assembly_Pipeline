dd192df7ec9a6102d2b85709fe233ba82f38f8cd
gbouras13 <84495559+gbouras13@users.noreply.github.com>
2022-06-04 12:27:06 +0930

    Update run_ghais.sh

--- slurm/run_ghais.sh
+++ slurm/run_ghais.sh
@@ -9,10 +9,11 @@
 #SBATCH --output="bact_assembly_snk.out"
 
 # Resources allocation request parameters
-#SBATCH -N 1                                                    # number of tasks (sequential job starts 1 task) (check this if your job unexpectedly uses 2 nodes)
-#SBATCH -c 1                                                    # number of cores (sequential job calls a multi-thread program that uses 8 cores)
-#SBATCH --time=2-23:00:00                                         # time allocation, which has the format (D-HH:MM), here set to 1 hou                                           # generic resource required (here requires 1 GPUs)
-#SBATCH --mem=1GB                                              # specify memory required per node
+#SBATCH -p batch
+#SBATCH -N 1                                  
+#SBATCH -c 1                                           
+#SBATCH --time=2-23:00:00                                                                          
+#SBATCH --mem=1GB                                             
 
 
 SNK_DIR="/hpcfs/users/a1667917/Bacteria_Multiplex/Nanopore_Bacterial_Assembly_Pipeline"
