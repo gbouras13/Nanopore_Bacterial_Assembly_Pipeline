e8e34d4334aaba3e21665e027b8dff3f9fd55c8e
gbouras13 <84495559+gbouras13@users.noreply.github.com>
2022-06-04 13:11:01 +0930

    Update run_ghais.sh

--- slurm/run_ghais.sh
+++ slurm/run_ghais.sh
@@ -22,7 +22,7 @@ PROF_DIR="/hpcfs/users/a1667917/snakemake_slurm_profile"
 cd $SNK_DIR
 
 module load Anaconda3/2020.07
-conda activate snakemake_clean_env
+conda activate snakemake_clean_m_env
 
 snakemake -c 1 -s runner.smk --use-conda --conda-frontend conda --profile $PROF_DIR/bact_assembly \
 --config csv=ghais_hpc_metadata.csv Output=/hpcfs/users/a1667917/Ghais/S_Aureus_Polished/Assembly_Output Polypolish_Dir=/hpcfs/users/a1667917/Polypolish min_chrom_length=2400000
