589452feaeca0042b6e2255f5897126672042958
gbouras13 <george.bouras@adelaide.edu.au>
2022-10-10 22:37:27 +1030

    snakemake profile

--- slurm/run_complete_staph.sh
+++ slurm/run_complete_staph.sh
@@ -19,17 +19,17 @@
 # run from Bacteria_Multiplex
 
 SNK_DIR="/hpcfs/users/a1667917/Bacteria_Multiplex/Nanopore_Bacterial_Assembly_Pipeline"
-PROF_DIR="/hpcfs/users/a1667917/snakemake_slurm_profile"
+PROF_DIR="$SNK_DIR/snakemake_profile"
 
 cd $SNK_DIR
 
 module load Anaconda3/2020.07
-conda activate snakemake_new
+conda activate snakemake_clean_env
 
 # snakemake -c 1 -s runner.smk --use-conda --profile $PROF_DIR/bact_assembly --conda-frontend conda --conda-create-envs-only \
 # --config csv=complete_metadata.csv Output=/hpcfs/users/a1667917/Staph_Final_Assemblies/Complete_Assembly_Output Polypolish_Dir=/hpcfs/users/a1667917/Polypolish min_chrom_length=2400000
 
-snakemake -c 1 -s runner.smk --use-conda  --conda-frontend conda --profile $PROF_DIR/bact_assembly  \
+snakemake -c 1 -s runner.smk --use-conda  --conda-frontend conda --profile $PROF_DIR/assembly  \
 --config csv=complete_metadata.csv Output=/hpcfs/users/a1667917/Staph_Final_Assemblies/Complete_Assembly_Output Polypolish_Dir=/hpcfs/users/a1667917/Polypolish min_chrom_length=2400000
 
 
