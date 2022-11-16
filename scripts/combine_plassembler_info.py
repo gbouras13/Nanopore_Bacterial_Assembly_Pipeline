fec9947ec51c1f99016296ef84bf1d12bd144a6b
gbouras13 <george.bouras@adelaide.edu.au>
2022-11-16 18:50:08 +1030

    plassembler summary6

--- scripts/combine_plassembler_info.py
+++ scripts/combine_plassembler_info.py
@@ -1,23 +1,34 @@
 #!/usr/bin/env python3
 
 import pandas as pd
+import os 
 
-def summarise_contigs(summary_list, output):
+def summarise_contigs(summary_list, output, sample):
     # read into list       
     summaries = []
     l =summary_list
 
     for a in l:
-        tmp_summary = pd.read_csv(a, delimiter= '\t', index_col=False, header=0)
-        # remove first row (from the file)
-        summaries.append(tmp_summary)
+        # only if > 0
+        if os.path.getsize(a) > 0:
+            tmp_summary = pd.read_csv(a, delimiter= '\t', index_col=False, header=0)
+            tmp_summary['Sample'] = sample
+
+            # shift column 'Sample' to first position
+            first_column = tmp_summary.pop('Sample')
+  
+            # insert column using insert(position,column_name,
+            # first_column) function
+            tmp_summary.insert(0, 'Sample', first_column)
+
+            summaries.append(tmp_summary)
 
     # make into combined dataframe
     total_summary_df = pd.concat(summaries,  ignore_index=True)
     total_summary_df.to_csv(output, sep=",", index=False)
         
 
-summarise_contigs(snakemake.input.summaries, snakemake.output.out)
+summarise_contigs(snakemake.input.summaries, snakemake.output.out, snakemake.wildcards.sample)
 
 
 
