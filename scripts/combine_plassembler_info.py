881676cc4f8b056e36bd70f10a92142cc0f4378c
gbouras13 <george.bouras@adelaide.edu.au>
2022-11-16 19:24:15 +1030

    plassembler summary13

--- scripts/combine_plassembler_info.py
+++ scripts/combine_plassembler_info.py
@@ -3,7 +3,7 @@
 import pandas as pd
 import os 
 
-def combine_sample_plassembler(summary_list, output, sample):
+def combine_sample_plassembler(summary_list, output):
     # read into list       
     summaries = []
     l =summary_list
@@ -12,15 +12,6 @@ def combine_sample_plassembler(summary_list, output, sample):
         # only if > 0
         if os.path.getsize(a) > 0:
             tmp_summary = pd.read_csv(a, delimiter= '\t', index_col=False, header=0)
-            tmp_summary['Sample'] = sample
-
-            # shift column 'Sample' to first position
-            first_column = tmp_summary.pop('Sample')
-  
-            # insert column using insert(position,column_name,
-            # first_column) function
-            tmp_summary.insert(0, 'Sample', first_column)
-
             summaries.append(tmp_summary)
 
     # make into combined dataframe
@@ -28,7 +19,7 @@ def combine_sample_plassembler(summary_list, output, sample):
     total_summary_df.to_csv(output, sep=",", index=False)
         
 
-combine_sample_plassembler(snakemake.input.summaries, snakemake.output.out, snakemake.wildcards.sample)
+combine_sample_plassembler(snakemake.input.summaries, snakemake.output.out)
 
 
 
